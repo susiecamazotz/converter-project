70674974e1dde44b25e9297104d5188bb214045a
Zuzanna Slapek <zuzekslapek@gmail.com>
2023-04-14 14:00:06 +0200

    JSON update

--- conversion_app/urls.py
+++ conversion_app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('dec-to-bin/', views.dec_to_bin, name='dec_to_bin_form'),
     path('dec-to-bin/<int:number>/', views.dec_to_bin, name='dec_to_bin'),
     path('hex-to-dec/', views.hex_to_dec, name='hex_to_dec'),
+    path('hex-to-dec/form/', views.hex_to_dec, name='hex_to_dec_form'),
 ]
--- conversion_app/views.py
+++ conversion_app/views.py
@@ -1,18 +1,32 @@
+from django.shortcuts import render
 from django.http import JsonResponse
 from rest_framework.decorators import api_view
 
 
-@api_view(['GET'])
-def dec_to_bin(request, number):
-    binary_number = format(number, 'b')
-    return JsonResponse(binary_number, safe=False)
-
-
-@api_view(['POST'])
-def hex_to_dec(request):
-    hex_number = request.data.get('hex', None)
-    if hex_number is not None:
-        decimal_number = int(hex_number, 16)
-        return JsonResponse({"dec": decimal_number})
+@api_view(['GET', 'POST'])
+def dec_to_bin(request, number=None):
+    if request.method == 'POST':
+        number = request.POST.get('dec', None)
+        if number is not None:
+            binary_number = format(int(number), 'b')
+            return render(request, 'dec_to_bin_form.html', {'bin_number': binary_number})
+        else:
+            return JsonResponse({"error": "No decimal value provided"}, status=400)
+    elif number is not None:
+        binary_number = format(number, 'b')
+        return JsonResponse(binary_number, safe=False)
     else:
-        return JsonResponse({"error": "No hex value provided"}, status=400)
+        return render(request, 'dec_to_bin_form.html')
+
+
+@api_view(['GET', 'POST'])
+def hex_to_dec(request):
+    if request.method == 'POST':
+        hex_number = request.POST.get('hex', None)
+        if hex_number is not None:
+            decimal_number = int(hex_number, 16)
+            return render(request, 'hex_to_dec_form.html', {'dec_number': decimal_number})
+        else:
+            return JsonResponse({"error": "No hex value provided"}, status=400)
+    else:
+        return render(request, 'hex_to_dec_form.html')
